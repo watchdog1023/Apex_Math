e51ca1a4ebdc2fe7a9997999f8917ff9ff0b623a
watchdog1023 <Naickercd@gmail.com>
2019-08-20 17:38:57 +0200

    Update apex_math.py

--- apex_math.py
+++ apex_math.py
@@ -4,7 +4,7 @@ import os
 pak_level = 45
 pak_bp_s1 = 11
 pak_bp_s2 = 12
-number_of_heirlooms = 1
+number_of_heirlooms = 2
 average_game_time_full = float((18+22)/2)
 average_game_time_half = float((11+13)/2)
 tier_up_half_game = 54000/1300
@@ -12,13 +12,15 @@ tier_up_full_game = 54000/3000
 total = pak_bp_s1 + pak_bp_s2 + pak_level
 chance = float((total/500)*100)
 wriath_heirloom_chance = float((1/number_of_heirlooms)*100)
+bloodhound_heirloom_chance = float((1/number_of_heirlooms)*100)
 
 print("Heirloom Chance:")
 print("Next pack opened is Heirloom: " + str(chance) + "%")
 print("Next pack will NOT be a Heirloom: " + str(100 - chance) + "%")
 print(" ")
 print("Chance that Heirloom will be:")
-print("Wriath's Kunai: "+ str(wriath_heirloom_chance) + "%")
+print("Wriath's Kunai: "+ str(wriath_heirloom_chance) + "%")]
+print("Bloodhound's Raven's Bite Axe: "+ str(bloodhound_heirloom_chance) + "%")
 print(" ")
 print("Battlepass S2 time to \"tier\" up:")
 print("Level 17 onwards: ")
