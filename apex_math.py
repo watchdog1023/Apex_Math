bca6cb6458ff098ec43ada0bf6636db4efddc797
watchdog1023 <Naickercd@gmail.com>
2019-10-06 21:23:17 +0200

    Update apex_math.py

--- apex_math.py
+++ apex_math.py
@@ -6,29 +6,34 @@ pak_bp_s1 = 11
 pak_bp_s2 = 12
 pak_bp_s3 = 12
 xp_per_sec = 3
+xp_per_kill = 50
+xp_per_dmg_point = 0.25
 number_of_heirlooms = 2
-average_game_time_full = float((18+22)/2)
-average_game_time_half = float((11+13)/2)
+average_game_time_full = float((18+21)/2) * 60
+average_game_time_half = float((10+12)/2) * 60
 tier_up_half_game = 54000/1300
 tier_up_full_game = 54000/3000
-total = pak_bp_s1 + pak_bp_s2 + pak_level
-chance = float((total/500)*100)
-wriath_heirloom_chance = float((1/number_of_heirlooms)*100)
-bloodhound_heirloom_chance = float((1/number_of_heirlooms)*100)
+total = pak_bp_s1 + pak_bp_s2 + pak_level + pak_bp_s3
+chance = float((total/500) * 100)
+wriath_heirloom_chance = float((1/number_of_heirlooms) * 100)
+bloodhound_heirloom_chance = float((1/number_of_heirlooms) * 100)
 
 print("Heirloom Chance:")
 print("Next pack opened is Heirloom: " + str(chance) + "%")
 print("Next pack will NOT be a Heirloom: " + str(100 - chance) + "%")
+print("*assuming you finished all three battlepasses,are level 100+ and you did not buy any other packs")
 print(" ")
 print("Chance that Heirloom will be:")
 print("Wriath's Kunai Knife: "+ str(wriath_heirloom_chance) + "%")
 print("Bloodhound's Raven's Bite Axe: "+ str(bloodhound_heirloom_chance) + "%")
 print(" ")
-'''print("Battlepass S2 time to \"tier\" up:")
-print("Level 17 onwards: ")
-print("Full Game(Top 3):")
-print(str(round(float(tier_up_full_game) * average_game_time_full,0)) + " Minutes")
-print(str(round(float(tier_up_full_game) * average_game_time_full,0)/60) + " Hours")
-print("Half Game(Top 10):")
-print(str(round(float(tier_up_half_game) * average_game_time_half,0)) + " Minutes")
-print(str(round(float(tier_up_half_game) * average_game_time_half,0)/60) + " Hours")'''
+print("XP earned:")
+print("Full game surival time XP earned(Top 3): "+ str(average_game_time_full * xp_per_sec) + " XP")
+print("Half game surival time XP earned(Top 10): "+ str(average_game_time_half * xp_per_sec) + " XP")
+print(" ")
+print("XP earned for killing a player:")
+print("without shield: " + str(xp_per_dmg_point * 100) + " XP")
+print("with white shield: " + str(xp_per_dmg_point * 150) + " XP")
+print("with blue shield: " + str(xp_per_dmg_point * 175) + " XP")
+print("with purple/gold shield: " + str(xp_per_dmg_point * 200) + " XP")
+print("*assuming that you don't third party and the player dose not heal")
