2268e208d5a63f24833c8964f6fed97f236bada0
watchdog1023 <Naickercd@gmail.com>
2019-11-06 18:02:27 +0200

    Update apex_math.py

--- apex_math.py
+++ apex_math.py
@@ -8,7 +8,7 @@ pak_bp_s3 = 12
 xp_per_sec = 3
 xp_per_kill = 50
 xp_per_dmg_point = 0.25
-number_of_heirlooms = 2
+number_of_heirlooms = 3
 average_game_time_full = float((18+21)/2) * 60
 average_game_time_half = float((10+12)/2) * 60
 tier_up_half_game = 54000/1300
@@ -17,6 +17,7 @@ total = pak_bp_s1 + pak_bp_s2 + pak_level + pak_bp_s3
 chance = float((total/500) * 100)
 wriath_heirloom_chance = float((1/number_of_heirlooms) * 100)
 bloodhound_heirloom_chance = float((1/number_of_heirlooms) * 100)
+lifeline_heirloom_chance = float((1/number_of_heirlooms) * 100)
 
 print("Heirloom Chance:")
 print("Next pack opened is Heirloom: " + str(chance) + "%")
@@ -26,6 +27,7 @@ print(" ")
 print("Chance that Heirloom will be:")
 print("Wriath's Kunai Knife: "+ str(wriath_heirloom_chance) + "%")
 print("Bloodhound's Raven's Bite Axe: "+ str(bloodhound_heirloom_chance) + "%")
+print("Lifeline's Shock Sticks Drumsticks: "+ str(lifeline_heirloom_chance) + "%")
 print(" ")
 print("XP earned:")
 print("Full game surival time XP earned(Top 3): "+ str(average_game_time_full * xp_per_sec) + " XP")
