502025c37d72dd885e75a18b5f75e27adcd398db
watchdog1023 <Naickercd@gmail.com>
2020-03-08 15:35:33 +0000

    updated

--- apex_math.py
+++ apex_math.py
@@ -1,24 +1,26 @@
 from __future__ import division
 import os
 
-pak_level = 45
+pak_level = 199
 pak_bp_s1 = 11
 pak_bp_s2 = 12
 pak_bp_s3 = 12
+pak_bp_s4 = 12
 xp_per_sec = 3
 xp_per_kill = 50
 xp_per_dmg_point = 0.25
-number_of_heirlooms = 4
+number_of_heirlooms = 5
 average_game_time_full = float((18+21)/2) * 60
 average_game_time_half = float((10+12)/2) * 60
 tier_up_half_game = 54000/1300
 tier_up_full_game = 54000/3000
-total = pak_bp_s1 + pak_bp_s2 + pak_level + pak_bp_s3
+total = pak_bp_s1 + pak_bp_s2 + pak_level + pak_bp_s3 + pak_bp_s4
 chance = float((total/500) * 100)
 wriath_heirloom_chance = float((1/number_of_heirlooms) * 100)
 bloodhound_heirloom_chance = float((1/number_of_heirlooms) * 100)
 lifeline_heirloom_chance = float((1/number_of_heirlooms) * 100)
 pathfinder_heirloom_chance = float((1/number_of_heirlooms) * 100)
+octane_heirloom_chance = float((1/number_of_heirlooms) * 100)
 
 print("Heirloom Chance:")
 print("Next pack opened is Heirloom: " + str(chance) + "%")
@@ -30,6 +32,7 @@ print("Wriath's Kunai Knife: "+ str(wriath_heirloom_chance) + "%")
 print("Bloodhound's Raven's Bite Axe: "+ str(bloodhound_heirloom_chance) + "%")
 print("Lifeline's Shock Sticks Drumsticks: "+ str(lifeline_heirloom_chance) + "%")
 print("Pathfinder's Boxing Gloves: "+ str(pathfinder_heirloom_chance) + "%")
+print("Octane's Butterfly Knife: "+ str(pathfinder_heirloom_chance) + "%")
 print(" ")
 print("XP earned:")
 print("Full game surival time XP earned(Top 3): "+ str(average_game_time_full * xp_per_sec) + " XP")
